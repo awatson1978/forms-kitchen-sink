f5b0c62acb7ec5b998803d5c9d5659b7726fae8d
Abigail Watson <awatson1978@gmail.com>
2013-05-28 02:36:35 -0400

    publish method finally worked out; inputs all working; publish!

--- client/libraries/app.form.js
+++ client/libraries/app.form.js
@@ -14,6 +14,15 @@ Session.set('editing_fax', false);
 Session.set('editing_email', false);
 Session.set('editing_web', false);
 
+Session.set('editing_password', false);
+Session.set('editing_date', false);
+Session.set('editing_birthdate', false);
+Session.set('editing_month', false);
+Session.set('editing_week', false);
+Session.set('editing_time', false);
+Session.set('editing_number', false);
+Session.set('editing_color', false);
+
 Session.set('is_deleting_task', false);
 
 //-------------------------------------------------------------
@@ -91,6 +100,41 @@ Template.formTemplate.events({
         Meteor.flush();
     },
 
+
+    'click #passwordInput':function(){
+        Session.set('editing_password', true);
+        Meteor.flush();
+    },
+    'click #dateInput':function(){
+        Session.set('editing_date', true);
+        Meteor.flush();
+    },
+    'click #birthdateInput':function(){
+        Session.set('editing_birthdate', true);
+        Meteor.flush();
+    },
+    'click #monthInput':function(){
+        Session.set('editing_month', true);
+        Meteor.flush();
+    },
+    'click #weekInput':function(){
+        Session.set('editing_week', true);
+        Meteor.flush();
+    },
+    'click #timeInput':function(){
+        Session.set('editing_time', true);
+        Meteor.flush();
+    },
+    'click #numberInput':function(){
+        Session.set('editing_number', true);
+        Meteor.flush();
+    },
+    'click #colorInput':function(){
+        Session.set('editing_color', true);
+        Meteor.flush();
+    },
+
+
     //-------------------------------------------------------------
     // 2. Mobile Tabs - Editing
 
@@ -141,7 +185,42 @@ Template.formTemplate.events({
     'mouseout #webInput':function(){
         Session.set('editing_web', false);
         Meteor.flush();
+    },
+
+
+    'mouseout #passwordInput':function(){
+        Session.set('editing_password', true);
+        Meteor.flush();
+    },
+    'mouseout #dateInput':function(){
+        Session.set('editing_date', true);
+        Meteor.flush();
+    },
+    'mouseout #birthdateInput':function(){
+        Session.set('editing_birthdate', true);
+        Meteor.flush();
+    },
+    'mouseout #monthInput':function(){
+        Session.set('editing_month', true);
+        Meteor.flush();
+    },
+    'mouseout #weekInput':function(){
+        Session.set('editing_week', true);
+        Meteor.flush();
+    },
+    'mouseout #timeInput':function(){
+        Session.set('editing_time', true);
+        Meteor.flush();
+    },
+    'mouseout #numberInput':function(){
+        Session.set('editing_number', true);
+        Meteor.flush();
+    },
+    'mouseout #colorInput':function(){
+        Session.set('editing_color', true);
+        Meteor.flush();
     }
+
 })
 
     //-------------------------------------------------------------
@@ -183,11 +262,11 @@ Template.formTemplate.events(
         {
             ok: function (value) {
                 CustomerAccounts.update(Session.get('selected_user'), {$set: { 'Company': value }});
-                Session.set('editing_company_name', false);
+                Session.set('editing_company', false);
                 Meteor.flush();
             },
             cancel: function () {
-                Session.set('editing_company_name', false);
+                Session.set('editing_company', false);
             }
         })
 );
@@ -196,11 +275,11 @@ Template.formTemplate.events(
         {
             ok: function (value) {
                 CustomerAccounts.update(Session.get('selected_user'), {$set: { 'Address': value }});
-                Session.set('editing_address_name', false);
+                Session.set('editing_address', false);
                 Meteor.flush();
             },
             cancel: function () {
-                Session.set('editing_address_name', false);
+                Session.set('editing_address', false);
             }
         })
 );
@@ -209,11 +288,11 @@ Template.formTemplate.events(
         {
             ok: function (value) {
                 CustomerAccounts.update(Session.get('selected_user'), {$set: { 'City': value }});
-                Session.set('editing_city_name', false);
+                Session.set('editing_city', false);
                 Meteor.flush();
             },
             cancel: function () {
-                Session.set('editing_city_name', false);
+                Session.set('editing_city', false);
             }
         })
 );
@@ -222,11 +301,11 @@ Template.formTemplate.events(
         {
             ok: function (value) {
                 CustomerAccounts.update(Session.get('selected_user'), {$set: { 'County': value }});
-                Session.set('editing_county_name', false);
+                Session.set('editing_county', false);
                 Meteor.flush();
             },
             cancel: function () {
-                Session.set('editing_county_name', false);
+                Session.set('editing_county', false);
             }
         })
 );
@@ -235,11 +314,11 @@ Template.formTemplate.events(
         {
             ok: function (value) {
                 CustomerAccounts.update(Session.get('selected_user'), {$set: { 'State': value }});
-                Session.set('editing_state_name', false);
+                Session.set('editing_state', false);
                 Meteor.flush();
             },
             cancel: function () {
-                Session.set('editing_state_name', false);
+                Session.set('editing_state', false);
             }
         })
 );
@@ -248,11 +327,11 @@ Template.formTemplate.events(
         {
             ok: function (value) {
                 CustomerAccounts.update(Session.get('selected_user'), {$set: { 'ZIP': value }});
-                Session.set('editing_zip_name', false);
+                Session.set('editing_zip', false);
                 Meteor.flush();
             },
             cancel: function () {
-                Session.set('editing_zip_name', false);
+                Session.set('editing_zip', false);
             }
         })
 );
@@ -261,11 +340,11 @@ Template.formTemplate.events(
         {
             ok: function (value) {
                 CustomerAccounts.update(Session.get('selected_user'), {$set: { 'Phone': value }});
-                Session.set('editing_phone_name', false);
+                Session.set('editing_phone', false);
                 Meteor.flush();
             },
             cancel: function () {
-                Session.set('editing_phone_name', false);
+                Session.set('editing_phone', false);
             }
         })
 );
@@ -274,11 +353,11 @@ Template.formTemplate.events(
         {
             ok: function (value) {
                 CustomerAccounts.update(Session.get('selected_user'), {$set: { 'Fax': value }});
-                Session.set('editing_fax_name', false);
+                Session.set('editing_fax', false);
                 Meteor.flush();
             },
             cancel: function () {
-                Session.set('editing_fax_name', false);
+                Session.set('editing_fax', false);
             }
         })
 );
@@ -287,11 +366,11 @@ Template.formTemplate.events(
         {
             ok: function (value) {
                 CustomerAccounts.update(Session.get('selected_user'), {$set: { 'Email': value }});
-                Session.set('editing_email_name', false);
+                Session.set('editing_email', false);
                 Meteor.flush();
             },
             cancel: function () {
-                Session.set('editing_email_name', false);
+                Session.set('editing_email', false);
             }
         })
 );
@@ -300,11 +379,118 @@ Template.formTemplate.events(
         {
             ok: function (value) {
                 CustomerAccounts.update(Session.get('selected_user'), {$set: { 'Web': value }});
-                Session.set('editing_web_name', false);
+                Session.set('editing_web', false);
                 Meteor.flush();
             },
             cancel: function () {
-                Session.set('editing_web_name', false);
+                Session.set('editing_web', false);
+            }
+        })
+);
+
+
+
+Template.formTemplate.events(
+    okCancelEvents('#passwordInput',
+        {
+            ok: function (value) {
+                CustomerAccounts.update(Session.get('selected_user'), {$set: { 'Password': value }});
+                Session.set('editing_password', false);
+                Meteor.flush();
+            },
+            cancel: function () {
+                Session.set('editing_password', false);
+            }
+        })
+);
+Template.formTemplate.events(
+    okCancelEvents('#dateInput',
+        {
+            ok: function (value) {
+                CustomerAccounts.update(Session.get('selected_user'), {$set: { 'Date': value }});
+                Session.set('editing_date', false);
+                Meteor.flush();
+            },
+            cancel: function () {
+                Session.set('editing_date', false);
+            }
+        })
+);
+Template.formTemplate.events(
+    okCancelEvents('#birthdateInput',
+        {
+            ok: function (value) {
+                CustomerAccounts.update(Session.get('selected_user'), {$set: { 'Birthdate': value }});
+                Session.set('editing_birthdate', false);
+                Meteor.flush();
+            },
+            cancel: function () {
+                Session.set('editing_birthdate', false);
+            }
+        })
+);
+Template.formTemplate.events(
+    okCancelEvents('#monthInput',
+        {
+            ok: function (value) {
+                CustomerAccounts.update(Session.get('selected_user'), {$set: { 'Month': value }});
+                Session.set('editing_month', false);
+                Meteor.flush();
+            },
+            cancel: function () {
+                Session.set('editing_month', false);
+            }
+        })
+);
+Template.formTemplate.events(
+    okCancelEvents('#weekInput',
+        {
+            ok: function (value) {
+                CustomerAccounts.update(Session.get('selected_user'), {$set: { 'Week': value }});
+                Session.set('editing_week', false);
+                Meteor.flush();
+            },
+            cancel: function () {
+                Session.set('editing_week', false);
+            }
+        })
+);
+Template.formTemplate.events(
+    okCancelEvents('#timeInput',
+        {
+            ok: function (value) {
+                CustomerAccounts.update(Session.get('selected_user'), {$set: { 'Time': value }});
+                Session.set('editing_time', false);
+                Meteor.flush();
+            },
+            cancel: function () {
+                Session.set('editing_time', false);
+            }
+        })
+);
+Template.formTemplate.events(
+    okCancelEvents('#numberInput',
+        {
+            ok: function (value) {
+                CustomerAccounts.update(Session.get('selected_user'), {$set: { 'Number': value }});
+                Session.set('editing_number', false);
+                Meteor.flush();
+            },
+            cancel: function () {
+                Session.set('editing_number', false);
+            }
+        })
+);
+Template.formTemplate.events(
+    okCancelEvents('#colorInput',
+        {
+            ok: function (value) {
+                CustomerAccounts.update(Session.get('selected_user'), {$set: { 'Color': value }});
+                Session.set('editing_color', false);
+                Meteor.flush();
+            },
+            cancel: function () {
+                Session.set('editing_color', false);
             }
         })
 );
@@ -421,6 +607,82 @@ Template.formTemplate.web_enabled = function(){
         return "readonly";
     }
 };
+
+
+Template.formTemplate.password_enabled = function(){
+    if(Session.get('global_edit')){
+        return "enabled";
+    }else if(Session.get('editing_password')){
+        return "enabled";
+    }else{
+        return "readonly";
+    }
+};
+Template.formTemplate.date_enabled = function(){
+    if(Session.get('global_edit')){
+        return "enabled";
+    }else if(Session.get('editing_date')){
+        return "enabled";
+    }else{
+        return "readonly";
+    }
+};
+Template.formTemplate.datetime_enabled = function(){
+    if(Session.get('global_edit')){
+        return "enabled";
+    }else if(Session.get('editing_datetime')){
+        return "enabled";
+    }else{
+        return "readonly";
+    }
+};
+Template.formTemplate.month_enabled = function(){
+    if(Session.get('global_edit')){
+        return "enabled";
+    }else if(Session.get('editing_month')){
+        return "enabled";
+    }else{
+        return "readonly";
+    }
+};
+Template.formTemplate.week_enabled = function(){
+    if(Session.get('global_edit')){
+        return "enabled";
+    }else if(Session.get('editing_week')){
+        return "enabled";
+    }else{
+        return "readonly";
+    }
+};
+Template.formTemplate.time_enabled = function(){
+    if(Session.get('global_edit')){
+        return "enabled";
+    }else if(Session.get('editing_time')){
+        return "enabled";
+    }else{
+        return "readonly";
+    }
+};
+Template.formTemplate.number_enabled = function(){
+    if(Session.get('global_edit')){
+        return "enabled";
+    }else if(Session.get('editing_number')){
+        return "enabled";
+    }else{
+        return "readonly";
+    }
+};
+Template.formTemplate.color_enabled = function(){
+    if(Session.get('global_edit')){
+        return "enabled";
+    }else if(Session.get('editing_color')){
+        return "enabled";
+    }else{
+        return "readonly";
+    }
+};
+
+
 Template.formTemplate.generic_enabled = function(){
     if(Session.get('global_edit')){
         return "enabled";
@@ -480,7 +742,16 @@ Template.formTemplate.events({
                     Phone: $('#phoneInput').val(),
                     Fax: $('#faxInput').val(),
                     Email: $('#emailInput').val(),
-                    Web: $('#webInput').val()
+                    Web: $('#webInput').val(),
+
+                    Password: $('#passwordInput').val(),
+                    Date: $('#dateInput').val(),
+                    Birthdate: $('#birthdateInput').val(),
+                    Month: $('#monthInput').val(),
+                    Week: $('#weekInput').val(),
+                    Time: $('#timeInput').val(),
+                    Number: $('#numberInput').val(),
+                    Color: $('#colorInput').val()
                 }, function (error, customer) {
                     console.log('error: ' + error);
                     console.log('customer: ' + customer);
